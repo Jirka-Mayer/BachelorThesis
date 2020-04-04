08b735351277acaba70a49d7a8c02422310e89a2
Jirka-Mayer <jirkamayer97@gmail.com>
2020-04-04 14:32:13 +0200

    Training on one, testing on the other

--- train_model.py
+++ train_model.py
@@ -6,9 +6,17 @@ from mashcima import Mashcima
 
 
 # build the dataset
-train_mc = Mashcima()
+train_mc = Mashcima([
+    "CVC-MUSCIMA_W-02_N-06_D-ideal.xml",
+    "CVC-MUSCIMA_W-02_N-13_D-ideal.xml",
+    "CVC-MUSCIMA_W-02_N-17_D-ideal.xml",
+])
 train_dataset = GeneratedDataset(size=2500, generator=lambda: generate(train_mc))
-dev_mc = Mashcima()
+dev_mc = Mashcima([
+    "CVC-MUSCIMA_W-01_N-10_D-ideal.xml",
+    "CVC-MUSCIMA_W-01_N-14_D-ideal.xml",
+    "CVC-MUSCIMA_W-01_N-19_D-ideal.xml",
+])
 dev_dataset = GeneratedDataset(size=100, generator=lambda: generate(dev_mc))
 
 # dev_dataset.check_dataset_visually()
