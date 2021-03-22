d5e7b5e7c3a8593f5d84ff069f79b9f7fae008b4
Jirka-Mayer <jirkamayer97@gmail.com>
2021-03-22 17:33:43 +0100

    Confusion matrix, basic

--- app/editops_levenshtein.py
+++ app/editops_levenshtein.py
@@ -42,3 +42,124 @@ def editops_levenshtein(gold: str, prediction: str):
     ops = Levenshtein.editops(prediction_encoded, gold_encoded)
 
     return [_transform_op(o, prediction_tokens, gold_tokens) for o in ops]
+
+
+def editops_levenshtein_sequenced(gold: str, prediction: str):
+    """Computes entire sequence-based edit operations, all replacements"""
+    # run levenshtein
+    gold_tokens = gold.split()
+    prediction_tokens = prediction.split()
+
+    gold_encoded = _encode_tokens(gold_tokens)
+    prediction_encoded = _encode_tokens(prediction_tokens)
+
+    # NOTE: editops(source, destination)
+    ops = Levenshtein.editops(prediction_encoded, gold_encoded)
+
+    # extract all operations that turn prediction to gold (add COPY operations)
+    # (COPY, REPLACE, DELETE, INSERT)
+
+    class Op:
+        def __init__(self, op_type, pred_from, pred_to, gold_from, gold_to):
+            self.op_type = op_type
+            self.pred_from = pred_from
+            self.pred_to = pred_to
+            self.gold_from = gold_from
+            self.gold_to =gold_to
+
+        def __repr__(self):
+            return f"Op({repr(self.op_type)}, {self.pred_from}, " + \
+                f"{self.pred_to}, {self.gold_from}, {self.gold_to})"
+
+    all_operations = []
+    prediction_pointer = 0
+    gold_pointer = 0
+    op_pointer = 0
+
+    def consume_op(op):
+        nonlocal prediction_pointer, gold_pointer
+        if op[0] == "insert":
+            all_operations.append(Op("insert",
+                prediction_pointer, prediction_pointer,
+                gold_pointer, gold_pointer + 1
+            ))
+            gold_pointer += 1
+        elif op[0] == "delete":
+            all_operations.append(Op("delete",
+                prediction_pointer, prediction_pointer + 1,
+                gold_pointer, gold_pointer
+            ))
+            prediction_pointer += 1
+        elif op[0] == "replace":
+            all_operations.append(Op("replace",
+                prediction_pointer, prediction_pointer + 1,
+                gold_pointer, gold_pointer + 1
+            ))
+            prediction_pointer += 1
+            gold_pointer += 1
+        else:
+            raise Exception("Unknown op type")
+
+    def attempt_copy():
+        nonlocal prediction_pointer, gold_pointer
+        assert prediction_tokens[prediction_pointer] == gold_tokens[gold_pointer]
+        all_operations.append(Op("copy",
+            prediction_pointer, prediction_pointer + 1,
+            gold_pointer, gold_pointer + 1
+        ))
+        prediction_pointer += 1
+        gold_pointer += 1
+
+    while op_pointer < len(ops) or \
+            prediction_pointer < len(prediction_tokens) or \
+            gold_pointer < len(gold_tokens):
+        if op_pointer < len(ops):
+            op = ops[op_pointer]
+            if op[1] == prediction_pointer and op[2] == gold_pointer:
+                # the next operation matches
+                consume_op(op)
+                op_pointer += 1
+            else:
+                # the next operation does not match
+                attempt_copy()
+        else:
+            # all operations have been consumed
+            attempt_copy()
+
+    # all operations should have been consumed and we are at the end
+    assert op_pointer == len(ops) \
+        and prediction_pointer == len(prediction_tokens) \
+        and gold_pointer == len(gold_tokens)
+
+    # get edit sequences
+    edit_sequences = []
+    sequence_start_index = None  # int when inside a running sequence
+    for index, op in enumerate(all_operations):
+        # no padding needed as it's already added at the very beginning
+
+        # start a sequence
+        if sequence_start_index is None and op.op_type != "copy":
+            sequence_start_index = index
+            continue
+        # end a sequence
+        if sequence_start_index is not None and op.op_type == "copy":
+            
+            # construct the sequence
+            from_sequence = [
+                prediction_tokens[j] for j in range(
+                    all_operations[sequence_start_index].pred_from,
+                    all_operations[index - 1].pred_to
+                )
+            ]
+            to_sequence = [
+                gold_tokens[j] for j in range(
+                    all_operations[sequence_start_index].gold_from,
+                    all_operations[index - 1].gold_to
+                )
+            ]
+            edit_sequences.append((from_sequence, to_sequence))
+            
+            sequence_start_index = None
+            continue
+
+    return edit_sequences
---
+++ confusion_matrix.py
@@ -0,0 +1,94 @@
+import json
+from app.muscima_annotations import MUSCIMA_RAW_ANNOTATIONS
+from app.editops_levenshtein import editops_levenshtein_sequenced
+from app.vocabulary import repair_annotation
+from app.vocabulary import to_generic
+from app.vocabulary import get_pitch
+from typing import List
+
+
+class Statistics:
+    JOINER = "  →  "
+    EMPTY_SEQUENCE = "∅"
+
+    def __init__(self, title, transformer=None):
+        # what to print on the report
+        self.title = title
+        
+        # transforms token sequences before aggregation
+        self.transformer = transformer
+
+        # internal statistics "seq": count
+        self.stats = {}
+
+    def add_replacements(self, replacements):
+        """Adds replacements right from the modified Levenshtein func"""
+        for r in replacements:
+            self.add_replacement(r[0], r[1])
+
+    def add_replacement(self, pred, gold):
+        if self.transformer is not None:
+            pred = self.transformer(pred)
+            gold = self.transformer(gold)
+
+        if len(pred) == 0:
+            pred_str = Statistics.EMPTY_SEQUENCE
+        else:
+            pred_str = " ".join(pred)
+
+        if len(gold) == 0:
+            gold_str = Statistics.EMPTY_SEQUENCE
+        else:
+            gold_str = " ".join(gold)
+        
+        key = pred_str + Statistics.JOINER + gold_str
+
+        if key not in self.stats:
+            self.stats[key] = 0
+        
+        self.stats[key] += 1
+
+    def print_report(self):
+        print()
+        print()
+        print("Replacement statistics: " + self.title)
+        print("-------------------------------------------------")
+        print("<count>: <replacement>")
+        for trans, count in sorted(self.stats.items(), key=lambda i: i[1], reverse=True):
+            print(str(count).rjust(7) + ": " + trans)
+
+
+def main():
+    print("We are investigating operations required to turn PREDICTION to GOLD")
+    print("(it's the inverse of the mistakes the model made)")
+
+    with open("experiment-predictions.json") as f:
+        prediction_sheet = json.load(f)
+
+    # TODO: transformer needs to run before levenshtein, not after
+
+    stats = [
+        Statistics("Basic stats"),
+        #Statistics("Generic tokens", lambda seq: [to_generic(t) for t in seq]),
+        #Statistics("Pitch only", lambda seq: [str(get_pitch(t)) for t in seq]),
+    ]
+
+    for writer, parts in MUSCIMA_RAW_ANNOTATIONS.items():
+        for part, staves in parts.items():
+            for i in range(len(staves)):
+                gold = MUSCIMA_RAW_ANNOTATIONS[writer][part][i]
+                prediction = prediction_sheet[str(writer)][str(part)][i]
+
+                replacements = editops_levenshtein_sequenced(
+                    repair_annotation(gold)[0],
+                    repair_annotation(prediction)[0]
+                )
+
+                for s in stats:
+                    s.add_replacements(replacements)
+
+    for s in stats:
+        s.print_report()
+
+
+main()
